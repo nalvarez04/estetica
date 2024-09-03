f390c23261818b11ab7119449e81c66fbec2cce4
nalvarez04 <nadiralvarez1292@gmail.com>
2024-09-03 12:58:15 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// document.getElementById('login').addEventListener('submit',(evt) =>{
+// 	let user = document.getElementById('usuario')
+// 	let pass = document.getElementById('pass')
+// 	if(user.value!="" || pass.value != ""){
+// 		window.location.href="main.html";
+// 		return true;
+// 	}else{
+// 		evt.preventDefault();
+// 	}
+// })
